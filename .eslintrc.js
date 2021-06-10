9d105e0e7d9f40862d47b8f1c62d75d090d2a0c9
code-xman <2991916069@qq.com>
2021-06-10 11:54:09 +0800

    关闭eslint引号验证

--- .eslintrc.js
+++ .eslintrc.js
@@ -67,6 +67,7 @@ module.exports = {
 
 
     // allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'quotes':'off',   // 关闭eslint引号验证
   }
 }
